1a5d4da2c7e4c025c49929175adcc3648feeb7c8
SCelisV <soniacelis@gmail.com>
2020-02-23 22:49:49 +0100

    OS module

---
+++ myProject02_os_nodejs/index.js
@@ -0,0 +1,11 @@
+// test del modulo os ->  The os module provides operating system-related utility methods and properties
+// https://nodejs.org/dist/latest-v12.x/docs/api/os.html
+const os = require('os');
+
+console.log(os.platform());
+
+console.log(os.release());
+
+console.log('free mem: ', os.freemem(), 'bytes')
+
+console.log('total mem: ', os.totalmem(), 'bytes')
