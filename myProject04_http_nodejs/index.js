b115a614af5aefd07360f41b9cdabf2fe5884fae
SCelisV <soniacelis@gmail.com>
2020-02-24 10:00:12 +0100

    add dependency express

--- myProject04_http_nodejs/index.js
+++ myProject04_http_nodejs/index.js
@@ -18,7 +18,7 @@ const http = require('http');
 
 const handleServer = function(req, res) {
     res.writeHead(200, {'Content-type': 'text/plain'}); //escribo la cabecera, indicandole la respuesta que le estoy dando. - https://es.wikipedia.org/wiki/Anexo:C%C3%B3digos_de_estado_HTTP
-    res.write('Hola mundoc - DEVUELVO 200');
+    res.write('Hola mundo - DEVUELVO 200');
     res.end();
 }  
 
@@ -32,6 +32,33 @@ server.listen(3000, function(){
 
 const colors = require('colors');
 
+//ficheros a subir -> npm install -> leer el package.json para instalar la aplicación y las dependencias.
+
+// modifico el package.json para cambiar el script de tal forma que cuando ejecute npm install se cree el servidor
+// "scripts": {
+// "start": "node index.js"
+// },
+
+// cuando ejecuto el comando npm start, se ejecutará la sección script del package.json
+// soniacelis@Sonias-MacBook-Pro myProject04_http_nodejs % npm start
+
+// > server@1.0.0 start /Users/soniacelis/SCProjects/0_SCProjects_github.com_SCelisV/node-js/myProject04_http_nodejs
+// > node index.js
+
+// Server on port 3000
+// 
 
 
+// para que se ejecute mi propio modulo "dev" tengo que colocarlo en el package.json
+// "scripts": {
+//     "start": "node index.js",
+//     "dev":"node index.js"
+//   },
 
+// y para ejecutarlo npm run dev
+// soniacelis@Sonias-MacBook-Pro myProject04_http_nodejs % npm run dev
+
+// > server@1.0.0 dev /Users/soniacelis/SCProjects/0_SCProjects_github.com_SCelisV/node-js/myProject04_http_nodejs
+// > node index.js
+
+// Server on port 3000
