c0d0c37f10bbe8b141de3faba83fe11a56ff3295
SCelisV <soniacelis@gmail.com>
2020-03-03 00:58:40 +0100

    Detailsv03 - sólo URLS ok

---
+++ myProject00_scraping/infocif/loadEmpresasDetailsDataset.js
@@ -0,0 +1,307 @@
+db = db.getSiblingDB("HOKKAIDODB");db.EmpresasDetails.drop();db.EmpresasDetails.insertMany([
+    {
+        "@context": "https://schema.org",
+        "@type": "Corporation",
+        "name":  "COFARES SOCIEDAD COOPERATIVA FARMAC�UTICA ESPA�OLA",
+        "alternateName": "COFARES SOCIEDAD COOPERATIVA FARMAC�UTICA ESPA�OLA",
+        "logo": "http://www.infocif.es/images/logo-empresas/nif-28140119.gif?v=1.01",
+        "image": "http://www.infocif.es/images/logo-empresas/nif-28140119.gif?v=1.01",
+        "foundingDate": "01/01/1944",
+        "foundingLocation": "MADRID",
+        "isicV4": "PRODUCTOS F�RMACOS / MEDICAMENTOS",
+        "numberOfEmployees": "0",
+        "taxID": "F28140119",
+        "telephone": "+34",
+        "description": "",
+        "identifier":
+                {
+                "@type": "PropertyValue",
+                "propertyID": "Company Number",
+                "value":  "F28140119"
+                },
+        "founder":
+              {
+              "@type": "Person",
+              "name": "",
+              "jobTitle": "",
+              "worksFor": {
+                          "@type": "Organization",
+                          "name": "COFARES SOCIEDAD COOPERATIVA FARMAC�UTICA ESPA�OLA"
+                          }
+               },
+        "contactPoint":
+              {
+              "@type": "ContactPoint",
+              "telephone": "+34",
+              "contactType": "customer service",
+              "areaServed": "ES",
+              "availableLanguage": "es"
+               },          
+        "url": "http://",
+        "address":
+              {
+              "@type": "PostalAddress",
+              "addressLocality": "MADRID",
+              "addressRegion": "Madrid",
+              "postalCode": "28010",
+              "streetAddress": "Calle Santa Engracia, 31"
+              }
+        }                    
+
+        ,
+
+    {
+        "@context": "https://schema.org",
+        "@type": "Corporation",
+        "name":  "VOLKSWAGEN NAVARRA SA",
+        "alternateName": "VOLKSWAGEN NAVARRA SA",
+        "logo": "http://www.infocif.es/images/logo-empresas/nif-31472459.gif?v=1.01",
+        "image": "http://www.infocif.es/images/logo-empresas/nif-31472459.gif?v=1.01",
+        "foundingDate": "28/12/1993",
+        "foundingLocation": "NAVARRA",
+        "isicV4": "AUTOMOTOR",
+        "numberOfEmployees": "4.764",
+        "taxID": "A31472459",
+        "telephone": "+34948424104",
+        "description": "Bienvenido a la P�gina Web de Volkswagen Navarra, S.A. Aqu� podr�s encontrar la informaci�n m�s relevante sobre nuestra empresa, as� como sobre nuestro producto: el Volkswagen Polo.",
+        "identifier":
+                {
+                "@type": "PropertyValue",
+                "propertyID": "Company Number",
+                "value":  "A31472459"
+                },
+        "founder":
+              {
+              "@type": "Person",
+              "name": "TOSTMANN ANDREAS",
+              "jobTitle": "Presidente",
+              "worksFor": {
+                          "@type": "Organization",
+                          "name": "VOLKSWAGEN NAVARRA SA"
+                          }
+               },
+        "contactPoint":
+              {
+              "@type": "ContactPoint",
+              "telephone": "+34948424104",
+              "contactType": "customer service",
+              "areaServed": "ES",
+              "availableLanguage": "es"
+               },          
+        "url": "http://www.vw-navarra.es",
+        "address":
+              {
+              "@type": "PostalAddress",
+              "addressLocality": "ARAZURI",
+              "addressRegion": "Navarra",
+              "postalCode": "31170",
+              "streetAddress": "POL�GONO INDUSTRIAL LANDABEN S/N"
+              }
+        }                    
+
+        ,
+
+    {
+        "@context": "https://schema.org",
+        "@type": "Corporation",
+        "name":  "AIRBUS DEFENCE AND SPACE SA",
+        "alternateName": "AIRBUS DEFENCE AND SPACE SA",
+        "logo": "http://www.infocif.es/images/logo-empresas/nif-28006104.gif?v=1.01",
+        "image": "http://www.infocif.es/images/logo-empresas/nif-28006104.gif?v=1.01",
+        "foundingDate": "01/01/1923",
+        "foundingLocation": "MADRID",
+        "isicV4": "AEROESPACIAL Y DEFENSA",
+        "numberOfEmployees": "8.220",
+        "taxID": "A28006104",
+        "telephone": "+34",
+        "description": "",
+        "identifier":
+                {
+                "@type": "PropertyValue",
+                "propertyID": "Company Number",
+                "value":  "A28006104"
+                },
+        "founder":
+              {
+              "@type": "Person",
+              "name": "GUTIERREZ MORENOALBERTO-JOSE",
+              "jobTitle": "Adm. Unico:",
+              "worksFor": {
+                          "@type": "Organization",
+                          "name": "AIRBUS DEFENCE AND SPACE SA"
+                          }
+               },
+        "contactPoint":
+              {
+              "@type": "ContactPoint",
+              "telephone": "+34",
+              "contactType": "customer service",
+              "areaServed": "ES",
+              "availableLanguage": "es"
+               },          
+        "url": "http://airbusdefenceandspace.com",
+        "address":
+              {
+              "@type": "PostalAddress",
+              "addressLocality": "GETAFE",
+              "addressRegion": "Madrid",
+              "postalCode": "28906",
+              "streetAddress": "Avenida John Lennon, s/n"
+              }
+        }                    
+
+        ,
+
+    {
+        "@context": "https://schema.org",
+        "@type": "Corporation",
+        "name":  "CRITERIA CAIXA SA",
+        "alternateName": "CRITERIA CAIXA SA",
+        "logo": "http://www.infocif.es/images/logo-empresas/nif-63379135.gif?v=1.01",
+        "image": "http://www.infocif.es/images/logo-empresas/nif-63379135.gif?v=1.01",
+        "foundingDate": "16/12/2003",
+        "foundingLocation": "BARCELONA",
+        "isicV4": "INTERMEDIARIOS FINANCIEROS",
+        "numberOfEmployees": "88",
+        "taxID": "A63379135",
+        "telephone": "+34934092751",
+        "description": "",
+        "identifier":
+                {
+                "@type": "PropertyValue",
+                "propertyID": "Company Number",
+                "value":  "A63379135"
+                },
+        "founder":
+              {
+              "@type": "Person",
+              "name": "FAINE CASAS ISIDRO",
+              "jobTitle": "Presidente",
+              "worksFor": {
+                          "@type": "Organization",
+                          "name": "CRITERIA CAIXA SA"
+                          }
+               },
+        "contactPoint":
+              {
+              "@type": "ContactPoint",
+              "telephone": "+34934092751",
+              "contactType": "customer service",
+              "areaServed": "ES",
+              "availableLanguage": "es"
+               },          
+        "url": "http://caixaholding.com",
+        "address":
+              {
+              "@type": "PostalAddress",
+              "addressLocality": "BARCELONA",
+              "addressRegion": "Barcelona",
+              "postalCode": "08028",
+              "streetAddress": " Av. Diagonal 621-629"
+              }
+        }                    
+
+        ,
+
+    {
+        "@context": "https://schema.org",
+        "@type": "Corporation",
+        "name":  "CD SUPPLY INNOVATION SL",
+        "alternateName": "CD SUPPLY INNOVATION SL",
+        "logo": "http://www.infocif.es/images/logo-empresas/nif-87922662.gif?v=1.01",
+        "image": "http://www.infocif.es/images/logo-empresas/nif-87922662.gif?v=1.01",
+        "foundingDate": "28/09/2017",
+        "foundingLocation": "MADRID",
+        "isicV4": "MINORISTAS DE ALIMENTOS / MEDICAMENTOS",
+        "numberOfEmployees": "8",
+        "taxID": "B87922662",
+        "telephone": "+34",
+        "description": "",
+        "identifier":
+                {
+                "@type": "PropertyValue",
+                "propertyID": "Company Number",
+                "value":  "B87922662"
+                },
+        "founder":
+              {
+              "@type": "Person",
+              "name": "BEGHIN OLIVIER",
+              "jobTitle": "Administrador Solidario",
+              "worksFor": {
+                          "@type": "Organization",
+                          "name": "CD SUPPLY INNOVATION SL"
+                          }
+               },
+        "contactPoint":
+              {
+              "@type": "ContactPoint",
+              "telephone": "+34",
+              "contactType": "customer service",
+              "areaServed": "ES",
+              "availableLanguage": "es"
+               },          
+        "url": "http://",
+        "address":
+              {
+              "@type": "PostalAddress",
+              "addressLocality": "POZUELO DE ALARC�N",
+              "addressRegion": "MADRID",
+              "postalCode": "28224",
+              "streetAddress": "CAMINO CERRO DE LOS GAMOS 1 - EDIFICIO 1"
+              }
+        }                    
+
+        ,
+
+    {
+        "@context": "https://schema.org",
+        "@type": "Corporation",
+        "name":  "MICHELIN ESPA�A PORTUGAL SA",
+        "alternateName": "MICHELIN ESPA�A PORTUGAL SA",
+        "logo": "http://www.infocif.es/images/logo-empresas/nif-20003570.gif?v=1.01",
+        "image": "http://www.infocif.es/images/logo-empresas/nif-20003570.gif?v=1.01",
+        "foundingDate": "03/09/1932",
+        "foundingLocation": "MADRID",
+        "isicV4": "AUTOMOTOR",
+        "numberOfEmployees": "7.878",
+        "taxID": "A20003570",
+        "telephone": "+34914105131",
+        "description": "",
+        "identifier":
+                {
+                "@type": "PropertyValue",
+                "propertyID": "Company Number",
+                "value":  "A20003570"
+                },
+        "founder":
+              {
+              "@type": "Person",
+              "name": "ROBINA ROSAT MARIA PAZ",
+              "jobTitle": "Presidente",
+              "worksFor": {
+                          "@type": "Organization",
+                          "name": "MICHELIN ESPA�A PORTUGAL SA"
+                          }
+               },
+        "contactPoint":
+              {
+              "@type": "ContactPoint",
+              "telephone": "+34914105131",
+              "contactType": "customer service",
+              "areaServed": "ES",
+              "availableLanguage": "es"
+               },          
+        "url": "http://www.michelin.es",
+        "address":
+              {
+              "@type": "PostalAddress",
+              "addressLocality": "TRES CANTOS",
+              "addressRegion": "Madrid",
+              "postalCode": "28760",
+              "streetAddress": "AVENIDA DE LOS ENCUARTES, 19"
+              }
+        }                    
+
+        ,
+]);
